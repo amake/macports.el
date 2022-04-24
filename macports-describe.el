1d9c3b9e372cb11052d762a966d74f83a484679e
Aaron Madlon-Kay <aaron@madlon-kay.com>
2022-04-24 22:32:32 +0900

    Add license

--- macports-describe.el
+++ macports-describe.el
@@ -6,6 +6,20 @@
 ;; Version: 0.1.0
 ;; URL: https://github.com/amake/.emacs.d
 ;; Package-Requires: ((emacs "28.1"))
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; macports-describe.el is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 3, or (at your option) any later version.
+;;
+;; flutter-gen.el is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+;; A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; macports-describe.el.  If not, see http://www.gnu.org/licenses.
 
 ;;; Commentary:
 
