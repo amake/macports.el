a92149bd91a957ca520bf55ce019a9e59d084a3b
Aaron Madlon-Kay <aaron@madlon-kay.com>
2022-05-12 21:02:00 +0900

    Split out `macports-select' tests

---
+++ test/macports-select-tests.el
@@ -0,0 +1,61 @@
+;;; macports-select-tests.el --- A porcelain for MacPorts  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 Aaron Madlon-Kay
+
+;; Author: Aaron Madlon-Kay
+;; Version: 0.1.0
+;; URL: https://github.com/amake/macports.el
+;; Package-Requires: ((emacs "25.1") (transient "0.1.0"))
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; macports-select-tests.el is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option) any later
+;; version.
+;;
+;; macports-select-tests.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+;; Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; macports-select-tests.el.  If not, see http://www.gnu.org/licenses.
+
+;;; Commentary:
+
+;; A porcelain for MacPorts: tests
+
+;;; Code:
+
+(require 'macports)
+(require 'transient)
+
+(ert-deftest macports-select-refresh-test ()
+  (cl-letf (((symbol-function #'shell-command-to-string)
+             (lambda (_) (concat "autofoo                 autofoo-1      autofoo-1 autofoo-2  none\n"
+                            "foo                     foo1           foo1 foo2 foo3 none\n"))))
+    (macports-select-refresh)
+    (should (equal '(("autofoo"
+                      ["autofoo" "autofoo-1" "autofoo-1 autofoo-2 none"])
+                     ("foo"
+                      ["foo" "foo1" "foo1 foo2 foo3 none"]))
+                   tabulated-list-entries))))
+
+(ert-deftest macports-select-refresh-test-empty ()
+  (cl-letf (((symbol-function #'shell-command-to-string)
+             (lambda (_) "\n")))
+    (macports-select-refresh)
+    (should (equal nil tabulated-list-entries))))
+
+(ert-deftest macports-select-refresh-test-custom-command ()
+  (cl-letf ((macports-command "foobar")
+            ((symbol-function #'shell-command-to-string)
+             (lambda (cmd)
+               (should (string-prefix-p "foobar " cmd))
+               "\n")))
+    (macports-select-refresh)))
+
+(provide 'macports-select-tests)
+;;; macports-select-tests.el ends here
